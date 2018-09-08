fae234ef83176a92544b8b5aea0db2562e6cbf80
Amanda Slayton <alslayton86@gmail.com>
2018-09-08 13:19:30 -0500

    testme

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            List<string> lastTwoPlanets = new List<string>() { "Uranus", "Neptune" }; 
+            planetList.AddRange(lastTwoPlanets);
+            List<string> otherPlanets = new List<string>() { "Earth", "Venus" };
+            planetList.Insert(1, otherPlanets[0]);
+            planetList.Insert(1, otherPlanets[1]);
+            planetList.Add("Pluto");
+            foreach (var item in planetList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
